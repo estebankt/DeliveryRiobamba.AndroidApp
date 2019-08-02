52babea46d0745a18e5e6a518c7701abaee18e92
jimpol <jpdavalos1@hotmail.com>
2019-08-02 14:46:48 -0500

    prueba de GIT chuma puto

--- app/src/main/java/com/example/proyecto_login/RegisterActivity.java
+++ app/src/main/java/com/example/proyecto_login/RegisterActivity.java
@@ -89,7 +89,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             CheckUsers ch = new CheckUsers();
             // El metodo check me devuelve un string con los posibles resultados:accept(Login Valido),no_accepted(Login invalido)
             // invalid_pass(password invalido), error (Error por JSON)
-            //importnt
+            //chumeo eres un puto 
             checker = ch.check(JSON_STRING,Mail,Password);
 
 
